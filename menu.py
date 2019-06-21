194e8cb07e9baad5b2ced96394738e00dc2c1805
Bokke <bokke1010@gmail.com>
2019-06-21 08:52:37 +0200

    Removed unused getClick and onClick functions from the text class

--- menu.py
+++ menu.py
@@ -28,7 +28,7 @@ class Text():
         self.font = freetype.Font(None, 20)
 
     def draw(self, color: tuple = (0  ,0  ,0  )):
-        pygame.draw.rect(self.screen, color, self.coords, 1)
+        pygame.draw.rect(self.screen, color, self.coords, 2)
         # First we calculate the central position of the Button
         # Then we get the size of the text we're rendering
         # Using these values, we calculate the top-left corner for that text to be centered
@@ -39,11 +39,3 @@ class Text():
 
         textPos = (int(bm[0]-0.5*ts[0]), int(bm[1]-0.5*ts[1]))
         self.font.render_to(self.screen, textPos, self.text, fgcolor=color)
-
-    def getClick(self, pos):
-        if self.coords[0] <= pos[0] <= self.coords[2] and self.coords[1] <= pos[1] <= self.coords[3]:
-            self.onClick()
-
-    def onClick(self):
-        o = self.object
-        eval(self.result)

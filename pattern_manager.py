79b38a6cf1e72ebe2e59a5a183cbe5458eedc8b7
Bokke <bokke1010@gmail.com>
2019-06-25 10:02:08 +0200

    Somehow these didn't get staged? Change the level que to use a deque (full of inserts, so probably not faster) Add new commands for JSON Rename add command to spw (spawner) internally for consistency with blt and pat Add preTime spawner support to JSON

--- pattern_manager.py
+++ pattern_manager.py
@@ -1,16 +1,17 @@
 import json
 import projectile
 from base import *
+from collections import deque
 
 class PatternManager():
 
     def __init__(self, screen):
         self.screen = screen
-        self.spawners = {}
-        self.que = []
+        self.bulletManagers = {}
+        self.que = deque()
         self.time = 0
         self.lc = 0
-        self.spawner = projectile.bulletSpawner(screen = screen, spawning = False)
+        self.bulletManager = projectile.BulletManager(screen = screen)
 
 
     def loadJson(self, fileName):
@@ -24,20 +25,33 @@ class PatternManager():
             for key, value in item.items():
                 if key == "spawner":
                     for name, command in value.items():
-                        self.que.append((scheduledTime, "add", name + ":" + str(self.lc), command))
+                        self.que.append((scheduledTime, "spw", name + ":" + str(self.lc), command))
                 if key == "bullet":
                     for command in value:
-                        self.que.append((scheduledTime, "bul", command))
+                        self._queAppend((scheduledTime, "bul", command))
+                if key == "pattern":
+                    for name, command in value.items():
+                        self._queAppend((scheduledTime, "pat", name + ":" + str(self.lc), command))
+                if key == "trigger":
+                    for command in value:
+                        self._queAppend((scheduledTime, "trg", name + ":" + str(self.lc)))
                 if key == "wait":
                     scheduledTime += value
                 if key == "del":
                     for delItem in value:
-                        self.que.append((scheduledTime, "del", delItem + ":" + str(self.lc)))
+                        self._queAppend((scheduledTime, "del", delItem + ":" + str(self.lc)))
 
         # Unique identifier to prevent levels overwriting each other
         self.lc += 1
-        # Sort que
-        self.que.sort()
+
+    def _queAppend(self, command):
+        if len(self.que) > 0 and self.que[-1][0] > command[0]:
+            i = len(self.que) - 1
+            while (self.que[i][0] > command[0]):
+                i -= 1
+            self.que.insert(i+1, command)
+        else:
+            self.que.append(command)
 
     def process(self, command):
         # command[0] is timing, often not required
@@ -45,66 +59,98 @@ class PatternManager():
         # command [2] is arg1 (optional, often name)
         # command [3] is arg2 (optional)
         # etc.
-        if command[1] == "add":
-            self.spawners[command[2]] = self.parseSpawner(command[3])
-            return True
+        if command[1] == "spw":
+            spawner = self.parseSpawner(command[3])
+            self.bulletManagers[command[2]] = spawner
         elif command[1] == "bul":
+            # Bullets do not have a name, so command[2] contains their parameters
             bullet = self.parseBullet(command[2])
-            self.spawner.addBullet(bullet)
+            self.bulletManager.addBullet(bullet)
+        elif command[1] == "pat":
+            pattern = self.parsePattern(command[3])
+            self.bulletManagers[command[2]] = pattern
+        elif command[1] == "trg":
+            self.bulletManagers[command[2]].trigger()
         elif command[1] == "del":
-            if self.spawners[command[2]].deleteSpawner():
-                self.spawners.pop(command[2], True)
+            if self.bulletManagers[command[2]].deleteManager():
+                self.bulletManagers.pop(command[2], True)
                 return True
             else:
                 return False
         elif command[1] == "end":
-            self.spawners = {}
+            self.bulletManagers = {}
             return True
 
 
     def parseSpawner(self, command):
         lt = -1
         slt = -1
+        pt = 0
         bdw = 3
         if "bulletLifeTime" in command:
             lt = command["bulletLifeTime"]
+        if "preTime" in command:
+            pt = command["preTime"]
         if "spawnerLifeTime" in command:
             slt = command["spawnerLifeTime"]
         if "borderWidth" in command:
             bdw = command["borderWidth"] # Borderwidth can be both a str as an int, so we assure the correct type is passed on later
 
-        spawner = projectile.bulletSpawner(screen=self.screen, spawningDelay=command["delay"], lifeTime = lt, spawnerLT = slt)
+        spawner = projectile.BulletSpawner(screen=self.screen, spawningDelay=command["delay"], lifeTime = lt, spawnerLT = slt)
 
         if command["type"] == "pointExp":
             spawner.setSpawningPointExp(coords=(command["sX"], command["sY"]), dir = command["bDir"], speed = command["speed"], size=command["size"], borderWidth=str(bdw))
         if command["type"] == "expBExp":
-            spawner.setSpawningExpBexp(coords=(command["sX"], command["sY"]), bulletPattern = (command["bX"], command["bY"]), size=command["size"], borderWidth=str(bdw))
+            spawner.setSpawningExpBexp(coords=(command["sX"], command["sY"]), bulletPath = (command["bX"], command["bY"]), size=command["size"], borderWidth=str(bdw))
         if command["type"] == "bExpAbs":
             spawner.setSpawningBexpAbs(coords=(command["bX"], command["bY"]), size=command["size"], borderWidth=str(bdw))
         return spawner
 
     def parseBullet(self, command):
-        preTime = 0
-        if "preTime" in command:
-            preTime = command["preTime"]
-        lifeTime = -1
-        if "lifeTime" in command:
-            lifeTime = command["lifeTime"]
+        lt = -1
+        pt = 0
         bdw = 3
+        if "bulletLifeTime" in command:
+            lt = command["bulletLifeTime"]
+        if "preTime" in command:
+            pt = command["preTime"]
         if "borderWidth" in command:
             bdw = command["borderWidth"]
-        bullet = projectile.Bullet(preTime = preTime, lifeTime = lifeTime)
+
+        bullet = projectile.Bullet(screen=self.screen, preTime = pt, lifeTime = lt)
         if command["type"] == "line":
-            bullet.setBulletPatternLine(pos = (command["x"], command["y"]), vel = (command["dx"], command["dy"]), size = command["size"], borderWidth = int(bdw))
+            bullet.setBulletPathLine(pos = (command["x"], command["y"]), vel = (command["dx"], command["dy"]), size = command["size"], borderWidth = int(bdw))
         if command["type"] == "expRel":
-            bullet.setBulletPatternExpRel(pos = (command["x"], command["y"]), vel = (command["dx"], command["dy"]), size = command["size"], borderWidth = str(bdw))
+            bullet.setBulletPathExpRel(pos = (command["x"], command["y"]), vel = (command["dx"], command["dy"]), size = command["size"], borderWidth = str(bdw))
         if command["type"] == "expAbs":
-            bullet.setBulletPatternExpAbs(pos = (command["x"], command["y"]), size = command["size"], borderWidth = str(bdw))
+            bullet.setBulletPathExpAbs(pos = (command["x"], command["y"]), size = command["size"], borderWidth = str(bdw))
 
         return bullet
 
+    def parsePattern(self, command):
+        lt = -1
+        pt = 0
+        bdw = 3
+        if "bulletLifeTime" in command:
+            lt = command["bulletLifeTime"]
+        if "preTime" in command:
+            pt = command["preTime"]
+        if "spawnerLifeTime" in command:
+            slt = command["spawnerLifeTime"]
+
+        pattern = projectile.BulletPattern(screen = self.screen, patternSize = command["count"])
+
+        if command["type"] == "pointExp":
+            pattern.setSpawningPointExp(coords=(command["sX"], command["sY"]), dir = command["bDir"], speed = command["speed"], size=command["size"], borderWidth=str(bdw))
+        elif command["type"] == "expBExp":
+            pattern.setSpawningExpBexp(coords=(command["sX"], command["sY"]), bulletPath = (command["bX"], command["bY"]), size=command["size"], borderWidth=str(bdw))
+        elif command["type"] == "bExpAbs":
+            pattern.setSpawningBexpAbs(coords=(command["bX"], command["bY"]), size=command["size"], borderWidth=str(bdw))
+
+        return pattern
+
     def add_pattern(self, pattern, name):
-        spawners[name] = pattern
+        bulletManagers[name] = pattern
 
     def update(self, dt, player):
         self.time += dt*1000
@@ -112,23 +158,23 @@ class PatternManager():
             while self.que[0][0] <= self.time:
                 print(self.que[0])
                 self.process(self.que[0])
-                self.que.pop(0)
+                self.que.popleft()
                 # Stop checking the que if it is empty now
                 if len(self.que) == 0:
                     break
 
-        # Update the integrated spawner used as parent for bullets created by a bullet command
-        self.spawner.update(dt, player)
-        # Since we decided to only remove spawners once they have no more bullets left, we have to check for that
+        # Update the integrated bulletManager used as parent for bullets created by a bullet command
+        self.bulletManager.update(dt, player)
+        # Since we decided to only remove bulletManagers once they have no more bullets left, we have to check for that
         deleteQue = set()
-        for key, spawner in self.spawners.items():
-            spawner.update(dt, player)
-            if spawner.delete and len(spawner.bullets) == 0:
+        for key, manager in self.bulletManagers.items():
+            manager.update(dt, player)
+            if manager.delete and len(manager.bullets) == 0:
                 deleteQue.add(key)
-        for key in deleteQue:
-            self.spawners.pop(key, True)
+        for item in deleteQue:
+            self.bulletManagers.pop(item, True)
 
     def draw(self):
-        self.spawner.draw()
-        for spawner in self.spawners:
-            self.spawners[spawner].draw()
+        self.bulletManager.draw()
+        for key, manager in self.bulletManagers.items():
+            manager.draw()
